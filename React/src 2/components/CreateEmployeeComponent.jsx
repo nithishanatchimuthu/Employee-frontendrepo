d6df9f70474cfde633c9060ff8120f3f9a511e6e
nithishanatchimuthu <126152288+nithishanatchimuthu@users.noreply.github.com>
2023-03-08 11:35:35 +0530

    Add files via upload

---
+++ React/src 2/components/CreateEmployeeComponent.jsx
@@ -0,0 +1,146 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+import { BsCheckCircleFill,BsFillXCircleFill } from "react-icons/bs";
+
+class CreateEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            // step 2
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            emailId: '',
+            img: '',
+            roll_no: 0
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.saveOrUpdateEmployee = this.saveOrUpdateEmployee.bind(this);
+    }
+
+    // step 3
+    componentDidMount(){
+
+        // step 4
+        if(this.state.id === '_add'){
+            return
+        }else{
+            EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
+                let employee = res.data;
+                this.setState({
+                    firstName: employee.firstName,
+                    lastName: employee.lastName,
+                    emailId : employee.emailId,
+                    img : employee.img,
+                    roll_no: employee.roll_no
+                });
+            });
+        }        
+    }
+    saveOrUpdateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName, 
+            lastName: this.state.lastName, 
+            emailId: this.state.emailId, 
+            img: this.state.img,
+            roll_no: this.state.roll_no};
+        console.log('employee => ' + JSON.stringify(employee));
+
+        // step 5
+        if(this.state.id === '_add'){
+            EmployeeService.createEmployee(employee).then(res =>{
+                this.props.history.push('/employees');
+            });
+        }else{
+            EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+                this.props.history.push('/employees');
+            });
+        }
+    }
+    
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+    changeImgHandler= (event) => {
+        this.setState({img: event.target.value});
+    }
+    changeRoll_noHandler= (event) => {
+        this.setState({roll_no: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    getTitle(){
+        if(this.state.id === '_add'){
+            return <h3 className="text-center p-4 m-2 text-info">Add</h3>
+        }else{
+            return <h3 className="text-center p-4 m-2 text-info">Update</h3>
+        }
+    }
+    render() {
+        return (
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3 cardshadow3">
+                                
+                                {
+                                    this.getTitle()
+                                }
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler} />
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email Id: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Img Url: </label>
+                                            <input placeholder="Img Url" name="img" className="form-control" 
+                                                value={this.state.img} onChange={this.changeImgHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Roll No: </label>
+                                            <input placeholder="Roll No" name="roll_no" className="form-control" 
+                                                value={this.state.roll_no} onChange={this.changeRoll_noHandler}/>
+                                        </div>
+                                        <div className='btn-group'>
+                                        <button className="btn btn-success w-auto" onClick={this.saveOrUpdateEmployee}><BsCheckCircleFill/> Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}><BsFillXCircleFill/> Cancel</button>
+                                        </div> 
+                                         
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+        )
+    }
+}
+
+export default CreateEmployeeComponent
---
+++ React/src 2/components/UpdateEmployeeComponent.jsx
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+
+class UpdateEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            emailId: '',
+            img:'',
+            roll_no:0
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.updateEmployee = this.updateEmployee.bind(this);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
+            let employee = res.data;
+            this.setState({
+                firstName: employee.firstName,
+                lastName: employee.lastName,
+                emailId : employee.emailId,
+                img : employee.img,
+                roll_no : employee.roll_no
+            });
+        });
+    }
+
+    updateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName,
+             lastName: this.state.lastName, 
+             emailId: this.state.emailId, 
+             img: this.state.img,
+             roll_no: this.state.roll_no};
+        console.log('employee => ' + JSON.stringify(employee));
+        console.log('id => ' + JSON.stringify(this.state.id));
+        EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+            this.props.history.push('/employees');
+        });
+    }
+    
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+    changeImgHandler= (event) => {
+        this.setState({img: event.target.value});
+    }
+    changeRoll_noHandler= (event) => {
+        this.setState({roll_no: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3 cardshadow3">
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email Id: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Img Url: </label>
+                                            <input placeholder="Img Url" name="Img" className="form-control" 
+                                                value={this.state.img} onChange={this.changeImgHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Roll No: </label>
+                                            <input placeholder="Roll No" name="roll_no" className="form-control" 
+                                                value={this.state.roll_no} onChange={this.changeRoll_noHandler}/>
+                                        </div>
+
+                                        <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+        )
+    }
+}
+
+export default UpdateEmployeeComponent
